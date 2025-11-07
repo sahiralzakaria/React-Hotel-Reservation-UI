503d72eb6084896459055a187ac4f0c982add520
sahir zakaria <sahir.alzakaria@gmail.com>
2025-11-07 11:29:45 +0300

    add header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,28 +5,31 @@ import { faBed, faCar, faPlane, faTaxi } from "@fortawesome/free-solid-svg-icons
 const Header = () => {
     return (
         <div className="header">
-            <div className="haederList">
-                <div className="headerItems">
-                    <FontAwesomeIcon icon={faBed} />
-                    <span>Stays</span>
-                </div>
-                <div className="headerItems">
-                    <FontAwesomeIcon icon={faPlane} />
-                    <span>Flights</span>
-                </div>
-                <div className="headerItems">
-                    <FontAwesomeIcon icon={faCar} />
-                    <span>Car Rentals</span>
-                </div>
-                <div className="headerItems">
-                    <FontAwesomeIcon icon={faBed} />
-                    <span>Attractions</span>
-                </div>
-                <div className="headerItems">
-                    <FontAwesomeIcon icon={faTaxi} />
-                    <span>Airport Taxis</span>
+            <div className="headerContainer">
+                <div className="headerList">
+                    <div className="headerListItem active" >
+                        <FontAwesomeIcon icon={faBed} />
+                        <span>Stays</span>
+                    </div>
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faPlane} />
+                        <span>Flights</span>
+                    </div>
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faCar} />
+                        <span>Car Rentals</span>
+                    </div>
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faBed} />
+                        <span>Attractions</span>
+                    </div>
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faTaxi} />
+                        <span>Airport Taxis</span>
+                    </div>
                 </div>
             </div>
+
         </div>
     )
 }
