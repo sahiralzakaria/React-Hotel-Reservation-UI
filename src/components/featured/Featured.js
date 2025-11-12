7420f357570a1babd6c84bc05851d49cb9a052b4
sahir zakaria <sahir.alzakaria@gmail.com>
2025-11-12 13:50:27 +0300

    add home title

--- src/components/featured/Featured.js
+++ src/components/featured/Featured.js
@@ -7,59 +7,60 @@ const Featured = () => {
                 <img src='/assets/Antalya.webp' alt='' className='featuredImg'></img>
                 <div className='featuredTitles'>
                     <h1>Antalya</h1>
-                    <h2>Mediterranean Beaches & Ancient Ruins</h2>
+                    <h2>532 properties</h2>
                 </div>
             </div>
             <div className='featuredItem'>
                 <img src='/assets/Artvin.webp' alt='' className='featuredImg'></img>
                 <div className='featuredTitles'>
                     <h1>Artvin</h1>
-                    <h2>Lush Green Highlands</h2>
+                    <h2>87 properties</h2>
                 </div>
             </div>
             <div className='featuredItem'>
                 <img src='/assets/Bodrum.webp' alt='' className='featuredImg'></img>
                 <div className='featuredTitles'>
                     <h1>Bodrum</h1>
-                    <h2>Vibrant Nightlife & Historic Castle</h2>
+                    <h2>245 properties</h2>
                 </div>
             </div>
             {/* <div className='featuredItem'>
                 <img src='/assets/Bursa.webp' alt='' className='featuredImg'></img>
                 <div className='featuredTitles'>
                     <h1>Bursa</h1>
-                    <h2>Ottoman Heritage & Ski Resorts</h2>
+                    <h2>163 properties</h2>
                 </div>
             </div>
             <div className='featuredItem'>
                 <img src='/assets/Isparta.webp' alt='' className='featuredImg'></img>
                 <div className='featuredTitles'>
                     <h1>Isparta</h1>
-                    <h2>Rose Valley & Lavender Fields</h2>
+                    <h2>54 properties</h2>
                 </div>
             </div>
             <div className='featuredItem'>
                 <img src='/assets/Istanbul.webp' alt='' className='featuredImg'></img>
                 <div className='featuredTitles'>
                     <h1>Istanbul</h1>
-                    <h2>Bridging Europe and Asia</h2>
+                    <h2>1120 properties</h2>
                 </div>
             </div>
             <div className='featuredItem'>
                 <img src='/assets/Sakarya.webp' alt='' className='featuredImg'></img>
                 <div className='featuredTitles'>
                     <h1>Sakarya</h1>
-                    <h2>Sapanca Lake & Nature</h2>
+                    <h2>97 properties</h2>
                 </div>
             </div>
             <div className='featuredItem'>
                 <img src='/assets/Trabzon.webp' alt='' className='featuredImg'></img>
                 <div className='featuredTitles'>
                     <h1>Trabzon</h1>
-                    <h2>Black Sea Coast & Monasteries</h2>
+                    <h2>214 properties</h2>
                 </div>
             </div> */}
         </div>
+
     )
 }
 
