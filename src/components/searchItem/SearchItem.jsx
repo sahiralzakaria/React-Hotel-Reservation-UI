6142a1844e6985fafb9ea8c9f29e9b47568258ac
sahir zakaria <sahir.alzakaria@gmail.com>
2025-11-13 16:13:09 +0300

    add style to search items

--- src/components/searchItem/SearchItem.jsx
+++ src/components/searchItem/SearchItem.jsx
@@ -19,7 +19,7 @@ const SearchItem = () => {
                     You can cancel later, so lock in this great price today!
                 </span>
             </div>
-            <div className="siDetails"></div>
+            <div className="siDetails">details</div>
         </div>
     )
 }
