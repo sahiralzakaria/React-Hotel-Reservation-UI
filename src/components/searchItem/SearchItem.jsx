714f15ef5b2d40e11a8a3a6e548b3cbcfaf191eb
sahir zakaria <sahir.alzakaria@gmail.com>
2025-11-13 16:00:57 +0300

    add search Items

---
+++ src/components/searchItem/SearchItem.jsx
@@ -0,0 +1,27 @@
+import './searchItem.css'
+
+const SearchItem = () => {
+    return (
+        <div className='searchItem'>
+            <img src='/assets/Trabzon.webp' alt="" className="siImg" />
+            <div className="siDesc">
+                <h1 className="siTitle">Tower Street Apartments</h1>
+                <span className="siDistance">500m from center</span>
+                <span className="siTaxiOp">Free airport taxi</span>
+                <span className="siSubtitle">
+                    Studio Apartment with Air conditioning
+                </span>
+                <span className="siFeatures">
+                    Entire studio • 1 bathroom • 21m² 1 full bed
+                </span>
+                <span className="siCancelOp">Free cancellation </span>
+                <span className="siCancelOpSubtitle">
+                    You can cancel later, so lock in this great price today!
+                </span>
+            </div>
+            <div className="siDetails"></div>
+        </div>
+    )
+}
+
+export default SearchItem
