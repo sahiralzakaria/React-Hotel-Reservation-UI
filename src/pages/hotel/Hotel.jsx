f196ee7c449c93a329543a6f48928281c17f83f3
sahir zakaria <sahir.alzakaria@gmail.com>
2025-11-14 12:18:46 +0300

    finish hotel container styles

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -3,6 +3,8 @@ import Navbar from '../../components/navbar/Navbar'
 import Header from '../../components/header/Header'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+import MailList from '../../components/mailList/MailList'
+import Footer from '../../components/footer/Footer'
 
 const Hotel = () => {
 
@@ -71,6 +73,8 @@ const Hotel = () => {
             </div>
           </div>
         </div>
+        <MailList />
+        <Footer />
       </div>
     </div>
   )
